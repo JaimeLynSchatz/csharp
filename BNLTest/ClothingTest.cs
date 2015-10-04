9fc61845b12d630d586c289acbd49a20ff60f1a7
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-10-04 12:05:04 -0700

    Rewriting tests

---
+++ BNLTest/ClothingTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BNL.Clothing;
+
+namespace BNLTest
+{
+    [TestClass]
+    public class ClothingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+                public Clothing shirt1 = new Clothing();
+            // Act
+
+            // Assert
+        }
+    }
+}
