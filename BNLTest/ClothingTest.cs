b3e763669056525756234608aa6ec7e06c2c1bb3
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-10-04 14:11:14 -0700

    added to test, need to separate, need to fix ClothingType

--- BNLTest/ClothingTest.cs
+++ BNLTest/ClothingTest.cs
@@ -17,11 +17,19 @@ namespace BNLTest
             shirt1.Size = "large";
             shirt1.Description = shirt1.Gender + " " + shirt1.Color + " " + shirt1.Size + " ClothingType here";
 
+            Clothing dress1 = new Clothing();
+            dress1.Color = "red";
+            dress1.Gender = "women's";
+            dress1.Size = "large";
+            dress1.Description = dress1.Gender + " " + dress1.Color + " " + dress1.Size + " ClothingType here";
+
             // Act
             // working this part out still
 
             // Assert
             Assert.AreEqual("men's white large ClothingType here", shirt1.Description);
+            Assert.AreEqual("women's red large ClothingType here", dress1.Description);
+            Assert.AreNotEqual(shirt1.Description, dress1.Description);
         }
     }
 }
