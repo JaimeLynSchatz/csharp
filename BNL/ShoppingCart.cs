0a047a0102a34ca1b39ed4114031b2da06fe154c
Jaime Lyn Schatz <jaimelynschatz@gmail.com>
2015-10-19 13:21:13 -0700

    wrote beginning of failing test for shopping cart

--- BNL/ShoppingCart.cs
+++ BNL/ShoppingCart.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BNL
 {
-    class ShoppingCart
+    public class ShoppingCart
     {
 
         public ShoppingCart(int shopperID)
--- BNLTest/ShoppingCartTest.cs
+++ BNLTest/ShoppingCartTest.cs
@@ -1,14 +1,44 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BNL;
 
 namespace BNLTest
 {
     [TestClass]
     public class ShoppingCartTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        ShoppingCart cart;
+
+        [ClassInitialize]
+        static public void SetupOnce(TestContext testContext)
         {
+            // Create database connection or other resource we'll need throughout
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // fetch data from database connection, initialize instances, etc.
+            cart = new ShoppingCart("empty");
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            // awesome stuff, close the file, etc.
+        }
+
+        [ClassCleanup]
+        static public void TearDownOnce()
+        {
+            // teardown the temp DB, etc.
+        }
+
+        [TestMethod]
+        public void CanAddItemsToShoppingCart()
+        {
+            cart.Add("inventory item here");
+
         }
     }
 }
